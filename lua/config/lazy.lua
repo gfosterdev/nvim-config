ba77f522d56ac0d0c21d8e3bcce5abaa7b101b22
George Foster <gfoster.dev@gmail.com>
2024-11-14 09:30:23 +0000

    feature: add luasnip and eslint

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
